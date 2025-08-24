9fc4cb5d8a10f994e20fa4b1a2c6b25eb6ad9247
José Luis Rojas Miranda <joseluisrojas.to@gmail.com>
2025-08-24 07:06:36 +0000

    la de poner el logo que te doy copiado en forma de imagen

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,8 +3,8 @@ export function Logo() {
   return (
     <div className="flex items-center justify-center bg-primary/20 text-primary p-2 rounded-lg aspect-square h-full w-auto">
         <svg
-            width="24"
-            height="24"
+            width="100"
+            height="100"
             viewBox="0 0 100 100"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
